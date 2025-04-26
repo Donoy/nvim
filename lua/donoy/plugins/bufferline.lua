bb81c46d666f615cc1fa293638422e4cd3a4e3e3
Donoy <>
2025-04-26 21:35:29 +0800

    opt buffer plugin make it mutil tabpage when open new file

--- lua/donoy/plugins/bufferline.lua
+++ lua/donoy/plugins/bufferline.lua
@@ -4,7 +4,16 @@ return {
   version = "*",
   opts = {
     options = {
-      mode = "tabs",
+      diagnostics = "nvim_lsp",
+      -- 左侧让出 nvim-tree 的位置
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "File Explorer",
+          highlight = "Directory",
+          text_align = "left",
+        },
+      },
     },
   },
 }
